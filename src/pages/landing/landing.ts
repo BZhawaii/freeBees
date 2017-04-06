a24860519fff242b2b753c8b5bb099ffcbcefd10
Richard Beese <richardbeese@Richards-MacBook-Air.local>
2017-04-06 13:15:24 -0600

    Initial Commit

---
+++ src/pages/landing/landing.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-landing',
+  templateUrl: 'landing.html'
+})  //closes component
+export class LandingPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }  //closes constructor
+
+}  //closes export class
---
+++ src/pages/settings/settings.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-landing',
+  templateUrl: 'settings.html'
+})  //closes component
+export class SettingsPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }  //closes constructor
+
+}  //closes export class
